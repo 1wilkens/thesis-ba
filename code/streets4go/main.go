0224803c53fa14b1fd8a0dac67b08816eacde59a
Florian Wilkens <floya@live.de>
2015-02-24 22:34:15 +0100

    Finished Go phase 1

--- code/streets4go/main.go
+++ code/streets4go/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/qedus/osmpbf"
+)
 
 func main() {
-	fmt.Printf("This is streets4go")
+	fmt.Println("This is streets4go")
+
+	osmPath := os.Args[1]
+	osmFile, err := os.Open(osmPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer osmFile.Close()
+
+	pbfReader := osmpbf.NewDecoder(osmFile)
+	//runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Printf("Opening osm file: %s\n", osmPath)
+	err = pbfReader.Start(runtime.GOMAXPROCS(-1)) // use several goroutines for faster decoding
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var nodes, ways, relations uint64
+	for {
+		obj, err := pbfReader.Decode()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		} else {
+			switch obj := obj.(type) {
+			case *osmpbf.Node:
+				nodes++
+				break
+			case *osmpbf.Way:
+				ways++
+				break
+			case *osmpbf.Relation:
+				relations++
+				break
+			default:
+				log.Fatalf("Found unknow type: %T\n", obj)
+				break
+			}
+		}
+	}
+	fmt.Printf("Found %d nodes, %d ways and %d relations in %s\n", nodes, ways, relations, osmPath)
 }
