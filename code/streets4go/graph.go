c9cf04c11ceb7cd8f335d6b957fb4cb49a1c3359
Florian Wilkens <mrfloya_github@outlook.com>
2015-03-21 23:21:19 +0100

    Finished Go phase 3

--- code/streets4go/graph.go
+++ code/streets4go/graph.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"container/heap"
+	"fmt"
+)
+
+const (
+	INFINITY = ^uint(0) // uint.MaxValue
 )
 
 type Node struct {
@@ -36,11 +41,11 @@ type DijkstraGraph struct {
 	parents []int
 }
 
-func NewGraph(cap int) *Graph {
+func NewGraph() *Graph {
 	g := new(Graph)
 
-	g.nodes = make([]Node, cap) // not strictly required but should help performance
-	g.edges = make([]Edge, cap)
+	g.nodes = *new([]Node)
+	g.edges = *new([]Edge)
 
 	g.nodeIdx = make(map[int64]int)
 	g.edgeIdx = make(map[int64]int)
@@ -48,20 +53,39 @@ func NewGraph(cap int) *Graph {
 	return g
 }
 
-func (g *Graph) AddNode(n Node) {
+func (g *Graph) AddNode(n *Node) {
 	g.nodeIdx[n.osmID] = len(g.nodes)
-	g.nodes = append(g.nodes, n)
+	g.nodes = append(g.nodes, *n)
 }
 
-func (g *Graph) AddEdge(n1, n2 int, e Edge) {
+func (g *Graph) AddEdge(n1, n2 int, e *Edge) {
 	// add edge
 	idx := len(g.edges)
-	g.edges = append(g.edges, e)
+	g.edges = append(g.edges, *e)
 	g.edgeIdx[e.osmID] = idx
 
 	// link up adjecents
+	if _, ok := g.nodes[n1].adj[n2]; ok {
+		fmt.Println("Adding duplicate edge")
+	}
 	g.nodes[n1].adj[n2] = idx
 	g.nodes[n2].adj[n1] = idx
+}
+
+func (g *Graph) Print() {
+	fmt.Printf("Nodes (%d):\n", len(g.nodes))
+	for n := range g.nodes {
+		node := g.nodes[n]
+		fmt.Printf("(osm: %d, id: %d)\n", node.osmID, g.nodeIdx[node.osmID])
+	}
+	fmt.Printf("\nEdges (%d):\n", len(g.edges))
+	for n := range g.nodes {
+		node := g.nodes[n]
+		for adj, e := range node.adj {
+			fmt.Printf("N%d -> N%d [label=%d];\n", node.osmID, g.nodes[adj].osmID, g.edges[e].drivingTime)
+		}
+	}
+	fmt.Println("")
 }
 
 func FromGraph(g *Graph) (dg *DijkstraGraph) {
@@ -75,7 +99,7 @@ func FromGraph(g *Graph) (dg *DijkstraGraph) {
 
 func (dg *DijkstraGraph) Dijkstra(start int) {
 	for i := range dg.g.nodes {
-		dg.dist[i] = ^uint(0) // uint.MaxValue
+		dg.dist[i] = INFINITY
 		dg.parents[i] = -1
 	}
 	dg.dist[start] = 0
--- code/streets4go/main.go
+++ code/streets4go/main.go
@@ -10,11 +10,27 @@ import (
 	"github.com/qedus/osmpbf"
 )
 
+const (
+	NODES = 10
+	EDGES = 15
+
+	MAX_INDEX_NODES = NODES - 1
+	MAX_WEIGTH      = 14
+)
+
 func main() {
 	fmt.Println("This is streets4go")
 
-	osmPath := os.Args[1]
-	osmFile, err := os.Open(osmPath)
+	//osmPath := os.Args[1]
+	//countOsm(osmPath)
+
+	testGraphStructure()
+
+	printPerformanceStatistics()
+}
+
+func countOsm(path string) {
+	osmFile, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +38,7 @@ func main() {
 
 	pbfReader := osmpbf.NewDecoder(osmFile)
 	//runtime.GOMAXPROCS(runtime.NumCPU())
-	fmt.Printf("Opening osm file: %s\n", osmPath)
+	fmt.Printf("Opening osm file: %s\n", path)
 	err = pbfReader.Start(runtime.GOMAXPROCS(-1)) // use several goroutines for faster decoding
 	if err != nil {
 		log.Fatal(err)
@@ -53,9 +69,44 @@ func main() {
 			}
 		}
 	}
-	fmt.Printf("Found %d nodes, %d ways and %d relations in %s\n", nodes, ways, relations, osmPath)
+	fmt.Printf("Found %d nodes, %d ways and %d relations in %s\n", nodes, ways, relations, path)
+}
 
+func printPerformanceStatistics() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
-	fmt.Printf("total heap usage: %d allocs, %d frees, %d bytes allocated", m.Mallocs, m.Frees, m.TotalAlloc)
+	fmt.Printf("total heap usage: %d allocs, %d frees, %d bytes allocated\n", m.Mallocs, m.Frees, m.TotalAlloc)
+}
+
+func testGraphStructure() {
+	g := NewGraph()
+	for i := 1; i <= NODES; i++ {
+		g.AddNode(&Node{osmID: int64(i * 5), adj: make(map[int]int)})
+	}
+	for i := 0; i < EDGES/2; i++ {
+		n1, n2, n3 := i%MAX_INDEX_NODES, (i+2)%MAX_INDEX_NODES, (i+5)%MAX_INDEX_NODES
+		g.AddEdge(n1, n2, &Edge{osmID: int64(i), drivingTime: uint((i % MAX_WEIGTH) + 1)})
+		g.AddEdge(n1, n3, &Edge{osmID: int64(i + EDGES), drivingTime: uint((i + 5%MAX_WEIGTH) + 1)})
+	}
+
+	dg := FromGraph(g)
+	dg.g.Print()
+
+	dg.Dijkstra(0)
+	s := &dg.g.nodes[0]
+
+	for i := 1; i < 10; i++ {
+		p := &dg.g.nodes[i]
+		if dg.dist[i] == INFINITY {
+			fmt.Printf("Skipping %d since its unreachable!\n", p.osmID)
+			continue
+		}
+		fmt.Printf("Started @ %d with distance: %d\n", p.osmID, dg.dist[i])
+		for s != p {
+			p = &dg.g.nodes[dg.parents[dg.g.nodeIdx[p.osmID]]]
+			fmt.Printf("Discovered %d\n", p.osmID)
+		}
+		fmt.Println("Finished!")
+	}
+
 }
